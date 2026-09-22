ead5ffbe1e98faa8da52e0876c8cd605ad412103
asdfdev0 <asdfdev0@proton.me>
2026-09-22 12:57:27 +0800

    Configure JSP000682 as default Lake target

--- lakefile.lean
+++ lakefile.lean
@@ -2,4 +2,4 @@ import Lake
 open Lake DSL
 package jsp000682
 require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "db584cd6d46c92f209a44c0f1c829460d327499d"
-lean_lib JSP000682
+@[default_target] lean_lib JSP000682
